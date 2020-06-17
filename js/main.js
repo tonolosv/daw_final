f1182359eaeb293a553ccd5bb3c1737a838e974c
Daniel Lopez <danielopezsv@gmail.com>
2020-06-16 23:29:58 -0600

    test1

--- js/main.js
+++ js/main.js
@@ -73,31 +73,31 @@ function list() {
 }
 
 let cajero = {
-    balance: 500,
-    retirar: function() {
-        let monto = parseInt(document.getElementById('retiro_id').value);
-        if (monto <= this.balance) {
-            this.balance -= monto;
+        balance: 500,
+        retirar: function() {
+            let monto = parseInt(document.getElementById('retiro_id').value);
+            if (monto <= this.balance) {
+                this.balance -= monto;
+                this.paintBalance();
+                document.getElementById('retiro_id').value = '';
+            } else alert(
+                'No tienes fondos suficientes');
+        },
+        ingresar: function() {
+            this.balance += parseInt(document.getElementById('ingresar_id').value);
             this.paintBalance();
-            document.getElementById('retiro_id').value = '';
-        } else alert(
-            'No tienes fondos suficientes');
-    },
-    ingresar: function() {
-        this.balance += parseInt(document.getElementById('ingresar_id').value);
-        this.paintBalance();
-        alert('Saldo agregado correctamente');
-        document.getElementById('ingresar_id').value = '';
-    },
-    paintBalance: function() {
-        let b = this.balance.toFixed(2);
-        document.getElementById('balance_id').innerHTML = '$' + b;
-    },
-}
-cajero.paintBalance();
+            alert('Saldo agregado correctamente');
+            document.getElementById('ingresar_id').value = '';
+        },
+        paintBalance: function() {
+            let b = this.balance.toFixed(2);
+            document.getElementById('balance_id').innerHTML = '$' + b;
+        },
+    }
+    // cajero.paintBalance();
 
-var doc = new jsPDF();
-doc.text("Hello world!", 20, 20);
-doc.text("This is client-side Javascript, pumping out a PDF.", 20, 30);
-doc.addPage("a6", "l");
-doc.text("Do you like that?", 20, 20);
+// var doc = new jsPDF();
+// doc.text("Hello world!", 20, 20);
+// doc.text("This is client-side Javascript, pumping out a PDF.", 20, 30);
+// doc.addPage("a6", "l");
+// doc.text("Do you like that?", 20, 20);
